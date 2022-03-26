22bdb972601693b4a76f76e897030d58bf375ba8
Woogster <john.m.wooley@gmail.com>
2022-03-26 02:53:32 -0500

    Finished welcome page- mobile responsiveness too!

--- wooley-portfolio/src/components/Welcome.js
+++ wooley-portfolio/src/components/Welcome.js
@@ -4,8 +4,8 @@ import Header from './Header'
 
 function Welcome() {
   return (
-    <div className='flex flex-col justify-center h-full bg-me bg-no-repeat bg-auto bg-center'>
-      <div className='h-1/3 mb-40 text-3xl text-center '>Welcome to My Portfolio</div>
+    <div className='flex flex-col justify-around h-full bg-me bg-no-repeat bg-auto bg-center md:bg-contain lg:justify-between'>
+      <div className='h-1/3 pb-40 text-4xl font-semibold tracking-widest text-center lg:p-24 lg:text-5xl transition-transform ease-in-out duration-700 hover:-translate-y-7'>Welcome to My Portfolio</div>
       <button type="button" 
       className="text-gray-900 
                 mx-auto
@@ -13,15 +13,25 @@ function Welcome() {
                 border 
                 border-gray-300 
                 focus:outline-none
-                hover:bg-gray-100 
+                hover:bg-gray-100
+                hover:scale-125
+                hover: transition
+                hover: ease-in-out
+                hover:duration-400 
+                active:bg-gray-600
+                active:text-white
                 focus:ring-4 
                 focus:ring-gray-200 
                 font-medium 
                 rounded-lg 
-                text-sm 
+                text-sm
+
                 px-2 
                 py-2 
                 mt-40 
+                lg:mb-40
+                lg:text-xl
+                lg:w-36
                 h-12
                 w-24
                 dark:bg-gray-800 
