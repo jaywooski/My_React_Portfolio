ef9aa55f7ecccf2b34f04b1fa7cde458649bba80
unknown <john.m.wooley@gmail.com>
2022-03-29 00:23:36 -0500

    Fixed navbar

--- wooley-portfolio/src/components/Header.js
+++ wooley-portfolio/src/components/Header.js
@@ -6,7 +6,6 @@ import Tester from './Tester'
 
 function Header() {
   return (
-<<<<<<< HEAD
     <div className='h-36 flex justify-center border-2 border-yellow-400'>
       <div className='hidden sm:flex w-full'>
         <Navbar/>
@@ -14,10 +13,6 @@ function Header() {
       <div className='text-center my-auto sm:hidden'>
         <Dropdown />
       </div>
-=======
-    <div className='h-48 border-2 border-yellow-400'>
-        <Navbar/>
->>>>>>> 2a7e436034c790ae3679347ca9a932ff8fe2e1a7
     </div>
   )
 }
--- wooley-portfolio/src/components/Navbar.js
+++ wooley-portfolio/src/components/Navbar.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Navbar() {
   return (
-<<<<<<< HEAD
     <div className='flex justify-around text-xl mt-auto p-2 w-full'>
         <Link to='/home'>
             <div className=' hover:bg-blue-700 hover:text-white active:bg-indigo-800 rounded-xl p-4 transition ease-in'>
@@ -22,34 +21,6 @@ function Navbar() {
                 Contact
             </div>
         </Link>
-=======
-    <div className='h-full flex flex-col text-center'>
-      
-        <button id="dropdownDefault" 
-            data-dropdown-toggle="dropdown" 
-            type="button"
-            className='text-dark'>
-            <FontAwesomeIcon icon="fa-solid fa-bars" />
-        </button>
-
-        <div id="dropdown" className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700" data-popper-placement="bottom">
-            <ul className="py-1 text-sm text-white dark:text-gray-200" aria-labelledby="dropdownDefault">
-                <li>
-                    <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                </li>
-                <li>
-                    <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                </li>
-                <li>
-                    <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                </li>
-            </ul>
-        </div>
-
->>>>>>> 2a7e436034c790ae3679347ca9a932ff8fe2e1a7
     </div>
   )
 }
