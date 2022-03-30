b43c5b1179216eb3f373c55849bac90e751b524e
unknown <john.m.wooley@gmail.com>
2022-03-30 01:43:03 -0500

    Finished About Me page

--- wooley-portfolio/src/components/AboutMe.js
+++ wooley-portfolio/src/components/AboutMe.js
@@ -7,11 +7,11 @@ function AboutMe() {
   return (
     <div className='h-full border-4 bg-center bg-me_about bg-cover bg-no-repeat '>
       <Header />
-        <div className='text-center text-xl text-white bg-blue-500 border border-4 border-blue-800 sticky top-28 z-10'>About Me</div>
-        <div className='h-3/5 lg:h-full py-1.5 flex flex-row flex-wrap overflow-auto '> 
+        <div className='text-center text-xl bg-blue-500 border border-4 border-blue-800 sticky top-28 z-10'>About Me</div>
+        <div className='h-3/5 lg:h-full py-1.5 md:mt-20 flex flex-row flex-wrap overflow-auto '> 
             {/* <div className='h-3/5 overflow-auto' > */}
               {/* <img src='./assets/IMG_3860.PNG' alt='Picture of me'/> */}
-              <p className='w-5/12 backdrop-blur lg:backdrop-invert p-2 tracking-widest sm:text-xl text-white font-sans lg:my-auto lg:backdrop-blur xl:h-60'>
+              <p className='w-5/12 backdrop-blur lg:backdrop-invert p-2 tracking-widest sm:text-xl text-sky-800 font-sans lg:my-auto lg:backdrop-blur xl:h-60'>
                   Hi, I'm John Wooley. I'm currently an up and coming full-stack developer. I'm looking for new opportunities in the job field to
                   become one of the best assets to an interested development team.
                   I'm from Nashville, Tennessee born and raised. I consider myself creative too but I'm always going 
