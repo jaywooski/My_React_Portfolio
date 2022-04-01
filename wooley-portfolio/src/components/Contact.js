beb54dee49e6b2b8d4895e785a4466d8f0b3ed0b
Woogster <john.m.wooley@gmail.com>
2022-03-31 23:31:12 -0500

    Finished, now to finish footer, then done!

--- wooley-portfolio/src/components/Contact.js
+++ wooley-portfolio/src/components/Contact.js
@@ -10,19 +10,19 @@ function Contact() {
       <div className='flex flex-col h-fit ' >
         {/* Main content goes here */}
         <div className='mx-auto py-auto  mt-40 text-xl text-white drop-shadow shadow-lg border border-4 bg-gradient-to-l from-blue-500 to-blue-900 rounded-lg w-1/2'>
-          Reach out to me through these provided links included in the footer and also through email as the link is provided here to below. Hope to hear from you soon!
+          Reach out to me through these provided links included in the footer and also through email as the link is provided here below too. Hope to hear from you soon!
         </div>
 
 
         <div className='flex flex-col mb-0 mx-auto mt-40 text-xl text-white drop-shadow shadow-lg border border-4 bg-gradient-to-l from-blue-500 to-blue-900 rounded-lg w-1/2' >
           <div className='flex justify-center bg-blue-500 p-12 border rounded-t-lg text-center tracking-wide text-2xl bg-gradient-to-r from-black to-blue-700 '>Contact Info</div>
           <div className='h-fit flex flex-col border rounded-b-lg flex-wrap justify-around'>
-            <ExternalLink href='mailto:John.M.Wooley@gmail.com' className='text-center px-auto py-4 active:bg-indigo-800 transition ease-in-out duration-300 hover:text-white hover:bg-blue-500'>Email</ExternalLink> 
+            <ExternalLink href='mailto:John.M.Wooley@gmail.com' className='text-center px-auto py-4 active:bg-indigo-800 transition ease-in-out duration-300 hover:text-white hover:bg-blue-500'>Email Me</ExternalLink> 
             <div className='flex flex-col text-center border-t text-center px-auto py-4 transition ease-in-out duration-300 hover:text-white hover:bg-blue-500'>
-              <div>Number</div>
+              <div>Call Me @</div>
               <div>615-772-3035</div>
             </div>
-            <ExternalLink className='text-center px-auto py-4 border-t active:bg-indigo-800 rounded-b-lg transition ease-in-out duration-300 hover:text-white hover:bg-blue-500' href='https://my.indeed.com/p/johnw-lv6zcmi/pdf'>Resume</ExternalLink>
+            <ExternalLink className='text-center px-auto py-4 border-t active:bg-indigo-800 rounded-b-lg transition ease-in-out duration-300 hover:text-white hover:bg-blue-500' href='https://my.indeed.com/p/johnw-lv6zcmi/pdf'>Download Resume</ExternalLink>
           </div>
         </div>
 
